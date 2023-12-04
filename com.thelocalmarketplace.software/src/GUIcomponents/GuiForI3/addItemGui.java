476ab0f3e70f8f3acbc5edf9b71f4c0407c87310
Justine <justinemangaliman@gmail.com>
2023-12-03 20:02:52 -0700

    implemented visual catalogue

--- com.thelocalmarketplace.software/src/GUIcomponents/GuiForI3/addItemGui.java
+++ com.thelocalmarketplace.software/src/GUIcomponents/GuiForI3/addItemGui.java
@@ -1,3 +1,26 @@
+// Aleksandr Sokolov (30191754)
+// Azariah Francisco (30085863)
+// Brandon Smith (30141515)
+// Carlos Serrouya (30192761)
+// Diego de Jaraiz (30176017)
+// Emily Willams (30122865)
+// Evan Ficzere (30192404)
+// Jaden Taylor (30113034)
+// Joshua Bourchier (30194364)
+// Justine Mangaliman (30164741)
+// Kaelin Good (30092239)
+// Laura Yang（30156356)
+// Myra Latif (30171760)
+// Noelle Thundathil (30115430)
+// Raj Rawat (30173990)
+// Roshan Patel (30184010)
+// Sam Fasakin (30161903)
+// Simon Bondad (30163401)
+// Simon Oseen (30144175)
+// Sohaib Zia (30160114)
+// Sunny Hoang (30170708)
+// Yasemin Khanmoradi (30066537)
+
 package GUIcomponents.GuiForI3;
 
 import java.awt.BorderLayout;
@@ -17,6 +40,7 @@ public class addItemGui extends Simulation implements ActionListener {
 	
 	public static final String SCAN_BARCODED_ITEM = "barcoded";
 	public static final String ADD_PLUCODED_ITEM = "plu";
+	public static final String ADD_VISUAL_CATALOGUE_ITEM = "visual";
 	private JFrame addItemGui;
 
 	addItemGui() {
@@ -47,6 +71,11 @@ public class addItemGui extends Simulation implements ActionListener {
 		addItembyitemlookupbutton.addActionListener(this);
 		panel.add(addItembyitemlookupbutton);
 		
+		Button addItemByVisualCatalogueButton = new Button("add item with visual catalogue"); // got to add on click start sesion and switch to main window
+		addItemByVisualCatalogueButton.setActionCommand(ADD_VISUAL_CATALOGUE_ITEM);
+		addItemByVisualCatalogueButton.addActionListener(this);
+		panel.add(addItemByVisualCatalogueButton);
+		
 		addItemGui.add(panel, BorderLayout.CENTER);
 	}
 
@@ -73,11 +102,10 @@ public class addItemGui extends Simulation implements ActionListener {
 			Simulation.station.getMainScanner().scan((BarcodedItem)Simulation.itemsToAdd.get(1).item);
 			addItemGui.dispose(); 
 			new MainGui();
+		} else if (command.equals(ADD_VISUAL_CATALOGUE_ITEM)) {
+			new VisualCatalogueGUI();
 		}
 		
 	}
 
 }
-
-
-
